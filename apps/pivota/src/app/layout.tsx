0dcef77b3e920a2d176afcc48eed73d5a8ab3da4
Allan Mathenge <allanmathenge67@gmail.com>
2025-07-16 11:34:17 +0300

    allan

---
+++ apps/pivota/src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from "@mantine/core";
+import Navbar from "../components/Navbar/Navbar";
+import { pivotaTheme } from '../../../../packages/ui/pivotaTheme';
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Pivota",
+  description: "Connecting Africa to Opportunities that matter",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <MantineProvider theme={pivotaTheme}>
+          <Navbar/>
+          {children}
+        </MantineProvider>
+        
+      </body>
+    </html>
+  );
+}
