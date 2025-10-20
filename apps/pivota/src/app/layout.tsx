f28b8fa466d04eb5a2c42c93f92c445ebdf5b4e5
Allan M <admin@EBLMICTHTGXHVWVXW.local>
2025-10-20 12:26:51 +0300

    added tawk

--- apps/pivota/src/app/layout.tsx
+++ apps/pivota/src/app/layout.tsx
@@ -65,24 +65,13 @@ export default function RootLayout({
           </MantineProvider>
         </StoreProvider>
 
-        {/* ✅Tawk.to Live Chat Script */}
-        <Script
-          id="tawk-to"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/67afa49512b6af190bdc75b3/1ik31lql9';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-          }}
-        />
+       {/*  Tawk.to Live Chat (Embed Script) */}
+            <Script
+              src="https://embed.tawk.to/67afa49512b6af190bdc75b3/1ik31lql9"
+              strategy="afterInteractive"
+              async={true}
+              crossOrigin="anonymous"
+            />
       </body>
     </html>
   );
