99f3d2658f23feacf3c5e48b63f33a84685c9d49
Allan M <admin@EBLMICTHTGXHVWVXW.local>
2025-10-20 13:28:35 +0300

    allan

--- apps/pivota/src/app/layout.tsx
+++ apps/pivota/src/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
           </MantineProvider>
         </StoreProvider>
 
-        {/* ✅ Tawk.to Live Chat Script */}
+        {/*  Tawk.to Live Chat Script */}
         <Script
           id="tawk-to"
           strategy="afterInteractive"
